b979f6dbc95f3d58e3b706a965eba17f7c9f661a
unsafe9 <realkorokke@outlook.com>
2025-09-17 23:45:05 +0900

    implement modal markdown rendering support

--- Templates/Scripts/modal.js
+++ Templates/Scripts/modal.js
@@ -1,26 +1,29 @@
 /**
  * Show a modal with the given title and content.
  * @param {string} title - The title of the modal.
- * @param {string} content - The content of the modal. It can contain line breaks (\n).
+ * @param {string} content - The content of the modal. It supports markdown.
  */
 function modal(tp, title, content) {
   class InfoModal extends tp.obsidian.Modal {
     constructor(title, content) {
       super(app);
-      this.modalTitle = title;
-      this.modalContent = content;
+      if (title) {
+        content = `# ${title}\n\n${content}`;
+      }
+      this.content = content;
+      this.component = new tp.obsidian.Component();
     }
 
     onOpen() {
       const { contentEl } = this;
 
       contentEl.empty();
-      contentEl.createEl("h2", { text: this.modalTitle });
-      contentEl.createEl("p", { text: this.modalContent, attr: { style: "white-space: pre-wrap;" } });
+      tp.obsidian.MarkdownRenderer.renderMarkdown(this.content, contentEl, '', this.component);
     }
 
     onClose() {
       this.contentEl.empty();
+      this.component.unload();
     }
   }
   new InfoModal(title, content).open();
